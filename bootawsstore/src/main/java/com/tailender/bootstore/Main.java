7f58eacf94164a4505d6704f2278fe79f25a7a44
Ashokkumar Subburaj <ashokkumar_subburaj@trimble.com>
2019-10-27 10:19:35 +0530

    Added profile sepeartor

--- bootawsstore/src/main/java/com/tailender/bootstore/Main.java
+++ bootawsstore/src/main/java/com/tailender/bootstore/Main.java
@@ -1,15 +1,25 @@
 package com.tailender.bootstore;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSCredentialsProviderChain;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Main implements CommandLineRunner {
 
-  @Value("${com.param}")
-  private String paramInFile;
+  @Value("${db.username}")
+  private String dbUserName;
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
@@ -17,6 +27,6 @@ public class Main implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("Param value is " + paramInFile);
+    System.out.println("Param value is " + dbUserName);
   }
 }
